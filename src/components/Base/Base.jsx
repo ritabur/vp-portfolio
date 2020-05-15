85d01dd743a73e6897770b0029f10a0d697fa9e5
RB <rita.bureikaite@tourlane.com>
2020-05-15 12:25:31 +0200

    update Base

--- src/components/Base/Base.jsx
+++ src/components/Base/Base.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import sanitizeHtml from 'sanitize-html';
 
 // not using p tag: https://github.com/gatsbyjs/gatsby/issues/11108
-const StyledBase = styled.span`
+const StyledBase = styled.div`
   font-size: ${props => props.theme.fontSizes.medium};
   line-height: 1.6;
 `;
