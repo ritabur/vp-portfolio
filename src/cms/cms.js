42d866a6d58ca7f74e1f2f4b984fc9e9dc59aa8f
RB <rita.bureikaite@tourlane.com>
2020-06-06 15:26:43 +0200

    test admin styles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,22 @@
 import CMS from 'netlify-cms-app';
 
+import withStyled from './withSC';
+
 import AboutPagePreview from './PreviewTemplates/AboutPagePreview';
 
-CMS.registerPreviewTemplate('about', AboutPagePreview);
+// admin styles not showing: https://github.com/netlify/netlify-cms/issues/793
+function smartRegisterPreviewTemplate(name, component) {
+    // check styled-components
+    try {
+        require("styled-components");
+
+        return CMS.registerPreviewTemplate(name, withStyled(component));
+    } catch (error) {
+        console.log('styled-components not loaded:', error);
+    }
+
+    // not using any css-in-js library
+    CMS.registerPreviewTemplate(name, component);
+}
+
+smartRegisterPreviewTemplate('about', AboutPagePreview);
